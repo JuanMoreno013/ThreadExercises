99458c3ad97d94481961863d795118d5b4880a6e
Juan Moreno <jmoreno@griddynamics.com>
2022-08-10 12:28:09 -0500

    Create Phone class to join the goods in a single type

---
+++ src/main/java/task2/version1/storageProducts/Phone.java
@@ -0,0 +1,13 @@
+package task2.version1.storageProducts;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Phone {
+
+    private Object phone;
+}
