2cb01cc51a55ca16cabec4a0fa9db2c1c9138d1f
Juan Moreno <jmoreno@griddynamics.com>
2022-08-10 12:29:28 -0500

    Create Storage class to store all the PhoneStorages

---
+++ src/main/java/task2/version1/storageProducts/Storage.java
@@ -0,0 +1,13 @@
+package task2.version1.storageProducts;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class Storage {
+    @Getter
+    private PhoneStorage[] storages;
+
+}
