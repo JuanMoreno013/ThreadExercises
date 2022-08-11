eb84d1fc2c372ae9d5583f3a900e1b1806d91be9
Juan Moreno <jmoreno@griddynamics.com>
2022-08-11 15:08:47 -0500

    Change implementation of Store

--- src/main/java/task2/version1/Store.java
+++ src/main/java/task2/version1/Store.java
@@ -1,5 +1,7 @@
 package task2.version1;
 
+import org.apache.log4j.BasicConfigurator;
+import task2.products.*;
 import task2.version1.storageProducts.*;
 
 import java.util.*;
@@ -8,37 +10,65 @@ import java.util.concurrent.Executors;
 
 
 public class Store {
+    private static final Map<Phone, Integer> productsMap = new HashMap<>();
+    private static final StorageQueue storeQueue = new StorageQueue(productsMap);
 
 
-    private static final ManageProductsMap MANAGE_PRODUCTS_MAP = new ManageProductsMap();
-    private static final Map<Object, Integer> productsMap = MANAGE_PRODUCTS_MAP.initializeProducts();
-    private static final StorageQueue storeProducts = new StorageQueue(productsMap);
+    private static final Apple apple = new Apple("Apple", "Iphone 13");
+    private static final Huawei huawei = new Huawei("Huawei", "Mate 40");
+    private static final LG lg = new LG("LG", "LG Velvet");
+    private static final Motorola motorola = new Motorola("Motorola", "Moto G9");
+    private static final Oppo oppo = new Oppo("Oppo", "Prime Z1");
+    private static final Samsung samsung = new Samsung("Samsung", "Samsung Galaxy S20");
+    private static final Sony sony = new Sony("Sony", "Xperia 30");
+    private static final Vivo vivo = new Vivo("Vivo", "Vivo Prime Z9");
+    private static final Xiaomi xiaomi = new Xiaomi("Xiaomi", "Mi 2s");
+    private static final ZTE zte = new ZTE("ZTE", "Ultimate ZTE 20");
+
+    public static void initializeMapProducts() {
+        int countProducts = 0;
+
+        productsMap.put(apple, countProducts);
+        productsMap.put(huawei, countProducts);
+        productsMap.put(lg, countProducts);
+        productsMap.put(motorola, countProducts);
+        productsMap.put(oppo, countProducts);
+        productsMap.put(samsung, countProducts);
+        productsMap.put(sony, countProducts);
+        productsMap.put(vivo, countProducts);
+        productsMap.put(xiaomi, countProducts);
+        productsMap.put(zte, countProducts);
+    }
 
     public static void main(String[] args) {
+        BasicConfigurator.configure();
 
+        initializeMapProducts();
 
-        PhoneStorage[] storages = new PhoneStorage[10];
-        storages[0] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[0]));
-        storages[1] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[1]));
-        storages[2] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[2]));
-        storages[3] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[3]));
-        storages[4] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[4]));
-        storages[5] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[5]));
-        storages[6] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[6]));
-        storages[7] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[7]));
-        storages[8] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[8]));
-        storages[9] = new PhoneStorage(new Phone(productsMap.keySet().toArray()[9]));
-        Storage storage = new Storage(storages);
+        PhoneStorage[] phoneStorages = new PhoneStorage[10];
+        phoneStorages[0] = new PhoneStorage(apple);
+        phoneStorages[1] = new PhoneStorage(huawei);
+        phoneStorages[2] = new PhoneStorage(lg);
+        phoneStorages[3] = new PhoneStorage(motorola);
+        phoneStorages[4] = new PhoneStorage(oppo);
+        phoneStorages[5] = new PhoneStorage(samsung);
+        phoneStorages[6] = new PhoneStorage(sony);
+        phoneStorages[7] = new PhoneStorage(vivo);
+        phoneStorages[8] = new PhoneStorage(xiaomi);
+        phoneStorages[9] = new PhoneStorage(zte);
+
 
         ExecutorService threadPoolProduce = Executors.newFixedThreadPool(5);
-        for (int i = 0; i < 10; i++)
-            threadPoolProduce.execute(new Producer(storeProducts, storage));
+        for (int i = 0; i < 5; i++)
+            threadPoolProduce.execute(new Producer(phoneStorages, storeQueue));
 
 
         ExecutorService threadPool = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 8; i++)
-            threadPool.execute(new Consumer(storeProducts));
+            threadPool.execute(new Consumer(storeQueue));
 
+
+//        productsMap.forEach((key, value) -> System.out.println("MAPAaaa " +key + " " + value));
     }
 
 
