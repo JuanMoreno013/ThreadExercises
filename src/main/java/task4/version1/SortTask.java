48637285c71982db79fef57602e682062997a04d
Juan Moreno <jmoreno@griddynamics.com>
2022-08-17 11:24:08 -0500

    Add numbers randoms to sort

--- src/main/java/task4/version1/SortTask.java
+++ src/main/java/task4/version1/SortTask.java
@@ -1,5 +1,6 @@
 package task4.version1;
 
+import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
 
 public class SortTask {
@@ -12,7 +13,14 @@ public class SortTask {
 
     public static void main(String[] args) {
 
-        int[] array = {1, 23, 34, 2, 4, 5, 5, 9, 80, 3, 10};
+//        int[] array = {1, 23, 34, 2, 4, 5, 5, 9, 80, 3, 10};
+
+        Random rand = new Random();
+        int[] array = new int[90];
+
+        for(int i = 0; i< array.length; i++){
+            array[i] = rand.nextInt(1000);
+        }
 
         long startTime = System.currentTimeMillis();
         parallelMergeSort(array);
